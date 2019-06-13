85ace56d93e1361e257640850e223b5aab19bf11
Release Notebook GK <andrea.mungari@gmail.com>
2019-06-13 18:45:53 +0200

    Ultime modifiche

--- projects/asw-895-docker-orchestrazione/d-sentence-stack-eureka-zuul/start-sentence-deployment.sh
+++ projects/asw-895-docker-orchestrazione/d-sentence-stack-eureka-zuul/start-sentence-deployment.sh
@@ -4,3 +4,5 @@ echo 'Starting sentence application as a stack'
 
 kubectl create secret generic regcred --from-file=.dockerconfigjson=/home/vagrant/.docker/config.json --type=kubernetes.io/dockerconfigjson
 kubectl apply -f sentence-stack.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+kubectl apply -f /home/asw/_shared/resources/load-balancer-configuration.yaml
